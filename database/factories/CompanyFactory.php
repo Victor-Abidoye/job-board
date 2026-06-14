b0a09398389537c6d2c08af0ff9a0241b34390e3
Victor Abidoye <abidoye8224@gmail.com>
2026-06-14 23:53:14 +0100

    add CompanyFactory for generating company model instances

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Company;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Company>
+ */
+class CompanyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'name' => fake()->company(),
+            'logo' => null,
+            'website' => fake()->url(),
+            'description' => fake()->text(50),
+            'location' => fake()->country()
+        ];
+    }
+}
